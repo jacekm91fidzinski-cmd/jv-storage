6f516557da7f875ba51c7998e92d813dd244c373
jacekm91fidzinski-cmd <jacekm91fidzinski@gmail.com>
2025-11-01 22:08:11 +0100

    jv-storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,56 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_CAPACITY = 10;
+
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+
+    public StorageImpl() {
+        keys = new Object[MAX_CAPACITY];
+        values = new Object[MAX_CAPACITY];
+        size = 0;
+    }
     @Override
     public void put(K key, V value) {
+        int index = indexOfKey(key);
+        if (index >= 0) {
+            values[index] = value;
+            return;
+        }
+        if (size < MAX_CAPACITY) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
+        int index = indexOfKey(key);
+        if (index >= 0) {
+            return (V) values[index];
+        }
         return null;
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+
+    private int indexOfKey(K key) {
+        for (int i = 0; i < size; i++) {
+            K currentKey = (K) keys[i];
+            if (currentKey == null && key == null) {
+                return i;
+            }
+            if (currentKey != null && currentKey.equals(key)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
